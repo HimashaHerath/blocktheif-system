cf5ba0016bf7d67dada546066b927153003bc57c
Himasha <himasha626@gmail.com>
2025-04-30 19:46:34 +0530

    comment out AboutUS export in section index

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -7,7 +7,7 @@ export { default as Hero } from "./heroSection";
 export { default as Footer } from "./footer";
 export { default as QR } from "./qrSection";
 export { default as Review } from "./reviewSection";
-export { default as AboutUS } from "./aboutUsSection";
+// export { default as AboutUS } from "./aboutUsSection";
 export { default as Manufacturer } from "./manuSection";
 export { default as Loader } from "./Loader";
 
